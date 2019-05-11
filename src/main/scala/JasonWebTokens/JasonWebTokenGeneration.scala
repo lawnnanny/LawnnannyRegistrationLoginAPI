3ea2d46fe80ed7115e6c4869528fa31f2cc1dbdc
shane <sdrafahl@iastate.edu>
2019-05-10 20:11:41 -0500

    Refactored

--- src/main/scala/JasonWebTokens/JasonWebTokenGeneration.scala
+++ src/main/scala/JasonWebTokens/JasonWebTokenGeneration.scala
@@ -4,19 +4,27 @@ import lambdas.config.UserSessionConfig
 import pdi.jwt.{JwtAlgorithm, JwtSprayJson}
 import spray.json.DefaultJsonProtocol._
 import spray.json._
-
 import scala.language.higherKinds
 
 case class LoginRequest(val username: String)
 
-class JasonWebTokenGenerator {
-    def encode(loginRequest: LoginRequest)(implicit userSessionConfig: UserSessionConfig): Option[String] = {
-        implicit val loginRequestFormatter = jsonFormat1(LoginRequest)
-        val algo = JwtAlgorithm.HS256
-        val jsObject = loginRequest.toJson.asJsObject
-        Some(JwtSprayJson.encode(jsObject, userSessionConfig.SECRET_KEY, algo))
+abstract class JasonWebTokenGenerator {
+    def encode(loginRequest: LoginRequest)(implicit userSessionConfig: UserSessionConfig): Option[String]
+}
+
+object JasonWebTokenGenerator {
+    def apply() : JasonWebTokenGenerator = {
+        new JasonWebTokenGenerator {
+            override def encode(loginRequest: LoginRequest)(implicit userSessionConfig: UserSessionConfig): Option[String] = {
+                implicit val loginRequestFormatter = jsonFormat1(LoginRequest)
+                val algo = JwtAlgorithm.HS256
+                val jsObject = loginRequest.toJson.asJsObject
+                Some(JwtSprayJson.encode(jsObject, userSessionConfig.SECRET_KEY, algo))
+            }
+        }
     }
 }
+
 object flyWeight {
-    implicit val jasonWebTokenGenerator = new JasonWebTokenGenerator
+    implicit val jasonWebTokenGenerator = JasonWebTokenGenerator()
 }
